c4909a52d21429cad32940622072c0bb593fb563
unknown <ashuzamaheshe4@gmail.com>
2022-07-22 10:39:10 +0200

    ash commit

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -36,9 +36,49 @@
 <body id="kt_body"
     class="header-fixed header-tablet-and-mobile-fixed toolbar-enabled toolbar-fixed aside-enabled aside-fixed"
     style="--kt-toolbar-height:55px;--kt-toolbar-height-tablet-and-mobile:55px">
-    <main class="py-4">
-        @yield('content')
-    </main>
+    <div class="d-flex flex-column flex-root">
+        <div class="page d-flex flex-row flex-column-fluid">
+            @include('layouts.partials._sideBar')
+            <div class="wrapper d-flex flex-column flex-row-fluid" id="kt_wrapper">
+                					<!--begin::Header-->
+					<div id="kt_header" style="" class="header align-items-stretch">
+                        						<!--begin::Container-->
+						<div class="container-fluid d-flex align-items-stretch justify-content-between">
+                            							<!--begin::Aside mobile toggle-->
+							<div class="d-flex align-items-center d-lg-none ms-n2 me-2" title="Show aside menu">
+								<div class="btn btn-icon btn-active-light-primary w-30px h-30px w-md-40px h-md-40px" id="kt_aside_mobile_toggle">
+									<!--begin::Svg Icon | path: icons/duotune/abstract/abs015.svg-->
+									<span class="svg-icon svg-icon-1">
+										<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+											<path d="M21 7H3C2.4 7 2 6.6 2 6V4C2 3.4 2.4 3 3 3H21C21.6 3 22 3.4 22 4V6C22 6.6 21.6 7 21 7Z" fill="currentColor" />
+											<path opacity="0.3" d="M21 14H3C2.4 14 2 13.6 2 13V11C2 10.4 2.4 10 3 10H21C21.6 10 22 10.4 22 11V13C22 13.6 21.6 14 21 14ZM22 20V18C22 17.4 21.6 17 21 17H3C2.4 17 2 17.4 2 18V20C2 20.6 2.4 21 3 21H21C21.6 21 22 20.6 22 20Z" fill="currentColor" />
+										</svg>
+									</span>
+									<!--end::Svg Icon-->
+								</div>
+							</div>
+							<!--end::Aside mobile toggle-->
+							<!--begin::Mobile logo-->
+							<div class="d-flex align-items-center flex-grow-1 flex-lg-grow-0">
+								<a href="../../demo1/dist/index.html" class="d-lg-none">
+									<img alt="Logo" src="assets/media/logos/logo-2.svg" class="h-30px" />
+								</a>
+							</div>
+                            <div class="d-flex align-items-stretch justify-content-between flex-lg-grow-1">
+                                @include('layouts.partials._topBar')
+                            </div>
+                        </div>
+                    </div>
+                    <div class="content d-flex flex-column flex-column-fluid" id="kt_content">
+                        @include('layouts.partials._toolbar')
+                        <main class="py-4">
+                            @yield('content')
+                        </main>
+                    </div>
+                    @include('layouts.partials._foot')
+            </div>
+        </div>
+    </div>
     <!--begin::Scrolltop-->
     <div id="kt_scrolltop" class="scrolltop" data-kt-scrolltop="true">
         <!--begin::Svg Icon | path: icons/duotune/arrows/arr066.svg-->
@@ -54,8 +94,6 @@
         <!--end::Svg Icon-->
     </div>
     <!--end::Scrolltop-->
-
-
     @livewireScripts
     <script>
         var hostUrl = "assets/";
