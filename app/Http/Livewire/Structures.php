3966d768e30112d7111addd70e697843f855863c
unknown <ashuzamaheshe4@gmail.com>
2022-07-22 15:40:16 +0200

    ash commit

--- app/Http/Livewire/Structures.php
+++ app/Http/Livewire/Structures.php
@@ -2,10 +2,63 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Structure;
 use Livewire\Component;
 
 class Structures extends Component
 {
+
+    public $codeStructure;
+    public $designation;
+    public $adresse_id;
+    public $avenu;
+    public $numParcelle;
+    public $long;
+    public $lat;
+    public $numTel1;
+    public $numTel2;
+    public $email;
+    public $siteWeb;
+    public $rccm;
+    public $idNational;
+    public $numImpot;
+    public $numCNSS;
+
+
+    public function save()
+    {
+        $validatedData = $this->validate([
+            'codeStructure'=>'required',
+            'designation'=>'required',
+            'adresse_id'=>'required',
+            'avenu'=>'required',
+            'numParcelle'=>'required',
+            'long'=>'required',
+            'lat'=>'required',
+            'numTel1'=>'required',
+            'numTel2'=>'required',
+            'email'=>'required',
+            'siteWeb'=>'required',
+            'rccm'=>'required',
+            'idNational'=>'required',
+            'numImpot'=>'required',
+            'numCNSS'=>'required'
+        ]);
+        try {
+            Structure::create($validatedData);
+            $this->emit('StudentUpdated');
+            $this->dispatchBrowserEvent('alert', [
+                'type' => 'success',
+                'message' => "Classe enregistée avec succes!!"
+            ]);
+        } catch (\Exception $e) {
+            $this->dispatchBrowserEvent('alert', [
+                'type' => 'error',
+                'message' => "Quelque chose ne va pas lors de l'enregistrement de la classe'!! " . $e->getMessage()
+            ]);
+        }
+    }
+
     public function render()
     {
         return view('livewire.structures');
