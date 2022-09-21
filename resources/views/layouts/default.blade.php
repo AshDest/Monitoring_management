c4e728e1b0a7ba2565c8973c6200d3e54cf3f139
unknown <ashuzamaheshe4@gmail.com>
2022-09-21 16:56:07 +0200

    ash commit

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -39,9 +39,15 @@
 <body id="kt_body"
     class="header-fixed header-tablet-and-mobile-fixed toolbar-enabled toolbar-fixed aside-enabled aside-fixed"
     style="--kt-toolbar-height:55px;--kt-toolbar-height-tablet-and-mobile:55px">
+        <!--begin::Main-->
+    <!--begin::Root-->
     <div class="d-flex flex-column flex-root">
+        <!--begin::Page-->
         <div class="page d-flex flex-row flex-column-fluid">
+            <!--begin::Aside-->
             @include('layouts.partials._sideBar')
+            <!--end::Aside-->
+            <!--begin::Wrapper-->
             <div class="wrapper d-flex flex-column flex-row-fluid" id="kt_wrapper">
                 <!--begin::Header-->
                 <div id="kt_header" style="" class="header align-items-stretch">
@@ -78,15 +84,36 @@
                         </div>
                     </div>
                 </div>
+                <!--end::Header-->
+                <!--begin::Content-->
                 <div class="content d-flex flex-column flex-column-fluid" id="kt_content">
+                    <!--begin::Toolbar-->
                     @include('layouts.partials._toolbar')
-                    <main class="py-4">
-                        @yield('content')
-                    </main>
+                    <!--end::Toolbar-->
+                    <!--begin::Post-->
+                    <div class="post d-flex flex-column-fluid" id="kt_post">
+                        <!--begin::Container-->
+                        <div id="kt_content_container" class="container-fluid">
+                            <!--begin::Contacts App- Add New Contact-->
+                            @yield('content')
+                            <!--end::Contacts App- Add New Contact-->
+                        </div>
+                        <!--end::Container-->
+                    </div>
+                    <!--end::Post-->
                 </div>
-                @include('layouts.partials._foot')
+                <!--end::Content-->
+                <!--begin::Footer-->
+                <div class="footer py-4 d-flex flex-lg-column" id="kt_footer">
+                    <!--begin::Container-->
+                    @include('layouts.partials._foot')
+                    <!--end::Container-->
+                </div>
+                <!--end::Footer-->
             </div>
+            <!--end::Wrapper-->
         </div>
+        <!--end::Page-->
     </div>
     <!--begin::Scrolltop-->
     <div id="kt_scrolltop" class="scrolltop" data-kt-scrolltop="true">
