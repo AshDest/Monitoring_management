c8b4c8908dcb0d38e2c9f61bfb68e20b8c8181d4
unknown <ashuzamaheshe4@gmail.com>
2022-10-13 22:45:14 +0200

    other updates

---
+++ app/Http/Livewire/Structure/EditStructure.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Structure;
+
+use Livewire\Component;
+
+class EditStructure extends Component
+{
+    public function render()
+    {
+        return view('livewire.structure.edit-structure');
+    }
+}
