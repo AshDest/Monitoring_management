834238fb87eb9f8cb10d32ff163b4350709e091b
unknown <ashuzamaheshe4@gmail.com>
2022-10-20 23:15:48 +0200

    ash commit

--- app/Models/GLAccount.php
+++ app/Models/GLAccount.php
@@ -8,4 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class GLAccount extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'code',
+        'description',
+        'balance',
+        'isAccount_system',
+        'account_type_id',
+        'account_level_id',
+        'currency_id',
+        'account_id'
+    ];
 }
