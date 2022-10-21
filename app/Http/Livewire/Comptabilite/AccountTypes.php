1428e8286d11c8e1f7099f2af70d791c83b6b3db
unknown <ashuzamaheshe4@gmail.com>
2022-10-21 14:25:15 +0200

    livewire files of new fonctions

---
+++ app/Http/Livewire/Comptabilite/AccountTypes.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Comptabilite;
+
+use Livewire\Component;
+
+class AccountTypes extends Component
+{
+    public function render()
+    {
+        return view('livewire.comptabilite.account-types');
+    }
+}
---
+++ app/Http/Livewire/Comptabilite/EditGlAccount.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Comptabilite;
+
+use Livewire\Component;
+
+class EditGlAccount extends Component
+{
+    public function render()
+    {
+        return view('livewire.comptabilite.edit-gl-account');
+    }
+}
